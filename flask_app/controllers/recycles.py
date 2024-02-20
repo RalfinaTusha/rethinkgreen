4b25ac137f5c6f0eebdeb1860948de4e987697e9
Flori-Bica <bicaflori135@gmail.com>
2024-02-20 15:50:21 +0100

    vlersim plerash

--- flask_app/controllers/recycles.py
+++ flask_app/controllers/recycles.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from flask_app import app
 from flask import render_template, request, session, redirect, flash
 from flask_app.models.recycle import Recycle
+from flask_app.models.user import User
 import os   
 from werkzeug.utils import secure_filename
 
@@ -21,6 +22,19 @@ def recycle():
         "items": request.form['totalItems'],
         "user_id": session['user_id']
     }
+    
+    if request.form['materialType'] == 'aluminum':
+        point_rc = 2 * int(request.form['totalItems'])
+    elif request.form['materialType'] == 'plastic':
+        point_rc = 0.5 * int(request.form['totalItems'])
+    elif request.form['materialType'] == 'paper':
+        point_rc = 1 * int(request.form['totalItems'])
+    elif request.form['materialType'] == 'other':
+        point_rc = 0.25 * int(request.form['totalItems'])
+    
+    user=User.get_by_id({'id':session['user_id']})
+    user_points = user.points
+    user_points += point_rc
     if 'image' in request.files:
         image = request.files['image']
         if image.filename != '':
@@ -28,6 +42,10 @@ def recycle():
             filename = secure_filename(image.filename)
             image.save(os.path.join(app.config['UPLOAD_FOLDER'], current_time + filename))
             data["image"] = current_time + filename
-    
+            data2 = {
+                "id": session['user_id'],
+                "points": user_points
+            }
+            User.update_points(data2)
             Recycle.save(data)
     return redirect('/')
